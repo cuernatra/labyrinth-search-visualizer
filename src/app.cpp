f96bee42be41f02e086726a6db478383564a2267
cuernatra <joel.turkka@gmail.com>
2026-03-07 17:02:17 +0200

    setup default files, folders and view

---
+++ src/app.cpp
@@ -0,0 +1,38 @@
+#include "app.h"
+
+App::App()
+    : window(sf::VideoMode(800, 600), "Labyrinth Search Visualizer")
+{
+}
+
+void App::run()
+{
+    while (window.isOpen())
+    {
+        processEvents();
+        update();
+        render();
+    }
+}
+
+void App::processEvents()
+{
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+        {
+            window.close();
+        }
+    }
+}
+
+void App::update()
+{
+}
+
+void App::render()
+{
+    window.clear();
+    window.display();
+}
